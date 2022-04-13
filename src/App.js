3884de40d516c205359f74eef1f65e7545591aab
philip howley <howley.phil@gmail.com>
2022-04-13 19:01:37 -0400

    Add React Router with 3 pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import "./App.css"
+import { Switch, Route, Redirect } from "react-router"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import Navbar from "./components/Navbar"
+import Home from "./pages/home.js"
+import About from "./pages/about.js"
+import Contact from "./pages/contact.js"
+import "./App.css"
 import { US_STATES } from "./data/us-states"
 
-// console.log(US_STATES)
 function App() {
   return (
     <div className='container'>
       <Header />
-      <div>one</div>
-      <div>two</div>
+      <Navbar />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/about' component={About} />
+        <Route exact path='/contact' component={Contact} />
+        <Redirect to='/' />
+      </Switch>
       <Footer year='1776' usStates={US_STATES} />
-      {/* <Footer year='2023' />
-      <Footer year='2022' /> */}
     </div>
   )
 }
